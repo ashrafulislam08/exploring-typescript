9911f38d298fc1fdb0d88bf5910ceb2f556ec56c
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-04-28 21:33:56 +0600

    learned about basic

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Master in TypeScript")
+const fullName: string = "Ashraful Islam"
+const age: number = 23;
+const isRainy: boolean = true;
+const person: object = {}
+const numbers: number[] = [25, 35, 45]
+
+
+
+// enum 
+
+enum Role {
+    Admin,
+    Manager,
+    User
+}
+
+enum isActive {
+    Active,
+    InActive
+}
